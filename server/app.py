7c243b72dc0f68ad4da32f6903236249b6f4d289
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-09-15 21:45:11 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,36 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+
+        record = Plant.query.filter_by(id=id).first()
+        for attr in request.json:
+            setattr(record, attr, request.json[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+    
+    def delete(self, id):
+
+        record = Plant.query.filter_by(id=id).first()
+        db.session.delete(record)
+        db.session.commit()
+
+
+        response = make_response(
+            "", 200
+        )
+
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
